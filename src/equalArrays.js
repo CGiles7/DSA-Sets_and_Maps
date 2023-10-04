4b15e13683f5b1e8ddb6197cfa1445f67f31b35c
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-04 13:36:51 -0400

    update functions

--- src/equalArrays.js
+++ src/equalArrays.js
@@ -1,6 +1,21 @@
 /**
  * return true if two arrays are equal, false otherwise
  */
-function isEqual(a1, a2) {}
+function isEqual(a1, a2) {
+    if (a1.length !== a2.length) return false;
+
+    let map = new Map();
+
+    for (let i = 0; i < a1.length; i++) {
+        map.set(a1[i], (map.get(a1[i]) || 0) + 1);
+        map.set(a2[i], (map.get(a2[i]) || 0) - 1);
+    }
+
+    for (let count of map.values()) {
+        if (count !== 0) return false;
+    }
+
+    return true;
+}
 
 module.exports = isEqual;
--- src/setOperations.js
+++ src/setOperations.js
@@ -1,16 +1,25 @@
 /**
  * return the union of two sets
  */
-function union(s1, s2) {}
+function union(s1, s2) {
+    let unionSet = new Set([...s1, ...s2]);
+    return unionSet;
+}
 
 /**
  * return the intersection of two sets
  */
-function intersect(s1, s2) {}
+function intersect(s1, s2) {
+    let intersectSet = new Set([...s1].filter(x => s2.has(x)));
+    return intersectSet;
+}
 
 /**
  * return the difference of two sets
  */
-function difference(s1, s2) {}
+function difference(s1, s2) {
+    let differenceSet = new Set([...s1].filter(x => !s2.has(x)));
+    return differenceSet;
+}
 
 module.exports = { union, intersect, difference };
